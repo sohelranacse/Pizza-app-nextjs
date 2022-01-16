d408a9249dccb220f7bc1bf14980d59e6841df24
Md. Sohel Rana <37040878+sohelranacse@users.noreply.github.com>
2022-01-16 07:54:49 +0600

    Updated PizzaCard.jsx

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -5,10 +5,10 @@ import Link from "next/link"
 const PizzaCard = ({ pizza }) => {  
   return (
     <div className={styles.container}>
-      <Link href={`/product/${pizza._id}`} passHref>
         <Image src={pizza.img} alt="" width="500" height="500" />
+      <Link href={`/product/${pizza._id}`} passHref>
+        <h1 className={styles.title}>{pizza.title}</h1>
       </Link>
-      <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza.prices[0]}</span>
       <p className={styles.desc}>
         {pizza.desc}
